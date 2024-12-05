301c663bdd8880b8bd90433dd1c04cb70efe7e27
Anish Karki <anishkarki5710@gmail.com>
2024-12-05 10:04:46 +0545

    Sets

---
+++ Codes/sets.py
@@ -0,0 +1,70 @@
+
+
+# Set is a collection of unordered and un-indexed distinct elements.
+# In Python set is used to store unique items, and it is possible to find the union, intersection, difference, symmetric difference, subset, super set and disjoint set among sets.
+
+
+
+#Creating sets
+st = {'item1', 'item2', 'item3', 'item4'}
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+
+
+
+#Checking a item is sets
+st = {'item1', 'item2', 'item3', 'item4'}
+print("Does set st contain item3? ", 'item3' in st) # Does set st contain item3? True
+
+
+
+#Adding items to set
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+fruits.add('lime')
+
+
+#Adding multiple item to sets
+st = {'item1', 'item2', 'item3', 'item4'}
+st.update(['item5','item6','item7'])
+
+
+#Converting list to set
+fruits = ['banana', 'orange', 'mango', 'lemon','orange', 'banana']
+fruits = set(fruits) # {'mango', 'lemon', 'banana', 'orange'}
+
+
+#Joining sets
+st1 = {'item1', 'item2', 'item3', 'item4'}
+st2 = {'item5', 'item6', 'item7', 'item8'}
+st3 = st1.union(st2)
+
+
+#Insertion of sets 
+st1 = {'item1', 'item2', 'item3', 'item4'}
+st2 = {'item5', 'item6', 'item7', 'item8'}
+st3 = st1.union(st2)
+
+
+#Subset and Superset
+st1 = {'item1', 'item2', 'item3', 'item4'}
+st2 = {'item2', 'item3'}
+st2.issubset(st1) # True
+st1.issuperset(st2) # True
+
+
+#Difference between two sets 
+whole_numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+even_numbers = {0, 2, 4, 6, 8, 10}
+whole_numbers.difference(even_numbers) # {1, 3, 5, 7, 9}
+
+python = {'p', 'y', 't', 'o','n'}
+dragon = {'d', 'r', 'a', 'g', 'o','n'}
+python.difference(dragon)     # {'p', 'y', 't'}  - the result is unordered (characteristic of sets)
+dragon.difference(python)     # {'d', 'r', 'a', 'g'}
+
+
+
+#Symmetric difference between two sets
+st1 = {'item1', 'item2', 'item3', 'item4'}
+st2 = {'item2', 'item3'}
+# it means (A\B)∪(B\A)
+st2.symmetric_difference(st1) # {'item1', 'item4'}
